c701f9be175e034e0e66c36f7a966712b86d1836
Dragoş Enache <155900245+DragosNH@users.noreply.github.com>
2026-05-27 08:55:10 +0200

    Added new file

---
+++ assets/js/pokemons.js
@@ -0,0 +1,14 @@
+export const pokedex = [
+    {
+        id: 1,
+        name: "Bulbasaur",
+        type1: "Grass",
+        type2: "Poisson",
+        weackness1: "Fire",
+        weackness2: "Flying",
+        weackness3: "Ice",
+        weackness4: "Psychic",
+        description: "For some time after its birth, it uses the nutrients that are packed into the seed on its back in order to grow.",
+        image: "assets/images/pokemons/1.png"
+    },
+]
